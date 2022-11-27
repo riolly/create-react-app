cf17522aa9685a9c369ff022746d682773230c21
riolly <alberto.riolly@gmail.com>
2022-11-28 00:03:58 +0700

    :lipstick: style: custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    extend: {
+      fontFamily: {
+        heading: ['Autour One', 'cursive'],
+        body: ['Abel', 'sans-serif'],
+        highlight: ['Architects Daughter', 'cursive'],
+      },
     extend: {},
+    },
   },
   plugins: [],
 }
