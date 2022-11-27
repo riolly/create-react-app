41f29080c7120a0fe201ef12ca204c73f6ab58fc
riolly <alberto.riolly@gmail.com>
2022-11-28 04:01:48 +0700

    :label: fix: router module types

--- src/router.ts
+++ src/router.ts
@@ -18,3 +18,10 @@ const routeConfig = rootRoute.addChildren([indexRoute, exampleRoute])
 const router = createReactRouter({routeConfig})
 
 export default router
+
+declare module '@tanstack/react-router' {
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	interface RegisterRouter {
+		router: typeof router
+	}
+}
