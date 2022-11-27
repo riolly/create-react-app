e9238fe250488ebba4b5a8e39c5d71a5a370f3ad
riolly <alberto.riolly@gmail.com>
2022-11-27 23:12:01 +0700

    :art: chore: prettier --write

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   useTabs: true,
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   bracketSpacing: false,
   jsxSingleQuote: true,
-  plugins: [require("prettier-plugin-tailwindcss")],
-  tailwindConfig: "./tailwind.config.cjs",
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.cjs',
   overrides: [
     {
-      files: [".*", "*.json", "*.md", "*.toml", "*.yml", "*.cjs", "*.mjs"],
+      files: ['.*', '*.json', '*.md', '*.toml', '*.yml', '*.cjs', '*.mjs'],
       options: {
         useTabs: false,
       },
     },
   ],
-};
+}
