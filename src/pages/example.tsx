d1aefab661b08af1fe44a67895fd4080f987943c
riolly <alberto.riolly@gmail.com>
2022-11-28 12:49:55 +0700

    :recycle: refactor: layout

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -3,12 +3,11 @@ import NavbarTopLayout from '../layouts/navbar-top'
 
 function ExamplePage() {
 	return (
-		<div className='min-h-screen bg-gradient-to-br from-primary-darkest to-dark-bg'>
-			<NavbarTopLayout />
-			<main className='flex flex-col items-center justify-center gap-8 px-0 pb-12 pt-12 md:gap-8 md:pt-4 md:pb-16'>
-				<h1 className='text-4xl'>Example page</h1>
-			</main>
-		</div>
+		<NavbarTopLayout>
+			<h1 className='flex items-center gap-0 bg-gradient-to-bl from-primary-darker to-primary-lighter bg-clip-text text-[2.75rem] font-extrabold text-transparent md:gap-2 md:text-[5rem]'>
+				Example Page
+			</h1>
+		</NavbarTopLayout>
 	)
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,77 +4,74 @@ import NavbarTopLayout from '../layouts/navbar-top'
 
 function HomePage() {
 	return (
-		<div className='min-h-screen bg-gradient-to-br from-primary-darkest to-dark-bg'>
-			<NavbarTopLayout />
-			<main className='flex flex-col items-center justify-center gap-8 px-0 pb-12 pt-12 md:gap-8 md:pt-4 md:pb-16'>
-				<div className='flex flex-col items-center'>
+		<NavbarTopLayout>
+			<div className='flex flex-col items-center'>
+				<a
+					href='https://github.com/riolly/create-react-app/'
+					target='_blank'
+					rel='noreferrer'
+					className=''
+				>
+					<h1 className='flex items-center gap-0 bg-gradient-to-bl from-primary-darker to-primary-lighter bg-clip-text text-[2.75rem] font-extrabold text-transparent md:gap-2 md:text-[5rem]'>
+						Create&nbsp;
+						<span>
+							<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
+								<img
+									src={reactLogo}
+									className='w-20 animate-spin'
+									alt='React'
+								/>
+							</a>
+						</span>
+						&nbsp;App
+					</h1>
+				</a>
+
+				<p className='mt-2 text-xl md:-mt-2 md:text-2xl'>
+					Riolly&apos;s opinion on building front-end app.
+				</p>
+
+				<p className='text-xl md:text-2xl'>
+					For full stack app check{' '}
 					<a
-						href='https://github.com/riolly/create-react-app/'
+						href='https://t3.riolly.vercel.app/'
+						className='font-highlight underline decoration-blue-300 underline-offset-4 transition-all hover:tracking-wider'
 						target='_blank'
 						rel='noreferrer'
-						className='text-[2.75rem] font-extrabold md:text-[5rem]'
 					>
-						<h1 className='flex items-center gap-0 bg-gradient-to-bl from-primary-darker to-primary-lighter bg-clip-text text-transparent md:gap-2'>
-							Create&nbsp;
-							<span>
-								<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-									<img
-										src={reactLogo}
-										className='w-20 animate-spin'
-										alt='React'
-									/>
-								</a>
-							</span>
-							&nbsp;App
-						</h1>
-					</a>
+						create-t3-app
+					</a>{' '}
+				</p>
+			</div>
 
-					<p className='mt-2 text-xl md:-mt-2 md:text-2xl'>
-						Riolly&apos;s opinion on building front-end app.
-					</p>
-
-					<p className='text-xl md:text-2xl'>
-						For full stack app check{' '}
-						<a
-							href='https://t3.riolly.vercel.app/'
-							className='font-highlight underline decoration-blue-300 underline-offset-4 transition-all hover:tracking-wider'
-							target='_blank'
-							rel='noreferrer'
-						>
-							create-t3-app
-						</a>{' '}
-					</p>
-				</div>
-
-				<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
-					<TechnologyCard
-						name='Typescript'
-						description='Stop using footgun (read javascript)'
-						documentation='https://www.typescriptlang.org/'
-					/>
-					<TechnologyCard
-						name='Vite'
-						description='Modern build tools for faster development.'
-						documentation='https://vitejs.dev/'
-					/>
-					<TechnologyCard
-						name='Tailwind'
-						description='Zen mode CSS. Styling feel like a breeze.'
-						documentation='https://tailwindcss.com/'
-					/>
-					<TechnologyCard
-						name='TanStack Query'
-						description='Data fetching, caching, mutation easy by default.'
-						documentation='https://tanstack.com/query/v4'
-					/>
-					<TechnologyCard
-						name='TanStack Router'
-						description='The first type-safe router human ever built with supercharge capability.'
-						documentation='https://tanstack.com/router'
-					/>
-				</div>
-			</main>
-		</div>
+			<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
+				<TechnologyCard
+					name='Typescript'
+					description='Stop using footgun (read javascript)'
+					documentation='https://www.typescriptlang.org/'
+				/>
+				<TechnologyCard
+					name='Vite'
+					description='Modern build tools for faster development.'
+					documentation='https://vitejs.dev/'
+				/>
+				<TechnologyCard
+					name='Tailwind'
+					description='Zen mode CSS. Styling feel like a breeze.'
+					documentation='https://tailwindcss.com/'
+				/>
+				<TechnologyCard
+					name='TanStack Query'
+					description='Data fetching, caching, mutation easy by default.'
+					documentation='https://tanstack.com/query/v4'
+				/>
+				<TechnologyCard
+					name='TanStack Router'
+					description='The first type-safe router human ever built with supercharge capability.'
+					documentation='https://tanstack.com/router'
+				/>
+			</div>
+		</NavbarTopLayout>
 	)
 }
 
